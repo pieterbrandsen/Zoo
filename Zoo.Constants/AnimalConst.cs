16569aec6128ebfcc512796f2be6725bc9d0f9de
Pieter <pieterbrandsen2@gmail.com>
2020-11-26 22:10:52 +0100

    AnimalDb is now encrypted, decrypted file is not saved

--- Zoo.Constants/AnimalConst.cs
+++ Zoo.Constants/AnimalConst.cs
@@ -19,5 +19,6 @@ namespace Zoo.Constants
     public class AnimalDbConst
     {
         public static string JsonFilePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/animalDb.txt";
+        public static string JsonEncryptedFilePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "/animalDbEncrypted.txt";
     }
 }
