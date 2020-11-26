a0548dd59327b2353bd9e490f84f9f5b7e90d87a
Pieter <pieterbrandsen2@gmail.com>
2020-11-26 13:37:51 +0100

    Added constants of classes

--- Zoo.Constants/Animals/AnimalsClassNames.cs
+++ Zoo.Constants/Animals/AnimalsClassNames.cs
@@ -6,6 +6,8 @@ namespace Zoo.Constants.Animals
 {
     public class AnimalsClassNames
     {
-        public const string 
+        public const string MONKEY = "Monkey";
+        public const string LION = "Lion";
+        public const string ELEPHANT = "Elephant";
     }
 }
